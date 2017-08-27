a58bfff6aae9fc54d056980ee5a476ca3f25bc9a
Chad Ellison <ChadEllison@Erins-iMac.local>
2017-08-26 19:07:52 -0600

    adjust config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              "smtp.sendgrid.net",
     port:                 587,
-    user_name:            ENV['SENDGRID_USERNAME'],,
+    user_name:            ENV['SENDGRID_USERNAME'],
     password:             ENV['SENDGRID_PASSWORD'],
     authentication:       'plain',
     domain:               'heroku.com',
