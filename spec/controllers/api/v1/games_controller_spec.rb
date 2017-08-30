eff49d44df66abe40c11de4a80d6c6b341d27650
Chad Ellison <ChadEllison@Erins-iMac.local>
2017-08-29 20:25:34 -0600

    add sad path

--- spec/controllers/api/v1/games_controller_spec.rb
+++ spec/controllers/api/v1/games_controller_spec.rb
@@ -40,29 +40,54 @@ RSpec.describe Api::V1::GamesController, type: :controller do
   end
 
   describe '#show' do
-    let(:email) { Faker::Internet.email }
-    let(:password) { 'password' }
-    let(:first_name) { Faker::Name.first_name }
-    let(:last_name) { Faker::Name.last_name }
-    let(:token) { 'token' }
+    context 'when the game exists' do
+      let(:email) { Faker::Internet.email }
+      let(:password) { 'password' }
+      let(:first_name) { Faker::Name.first_name }
+      let(:last_name) { Faker::Name.last_name }
+      let(:token) { 'token' }
 
-    let!(:user) {
-      User.create(email: email,
-      password: password,
-      firstName: first_name,
-      lastName: last_name,
-      approved: true,
-      token: token)
-    }
+      let!(:user) {
+        User.create(email: email,
+        password: password,
+        firstName: first_name,
+        lastName: last_name,
+        approved: true,
+        token: token)
+      }
 
-    let!(:game) { user.games.create }
+      let!(:game) { user.games.create }
 
-    it 'returns a user\'s serialize_game' do
-      get :show, params: { id: game.id, token: user.token }, format: :json
+      it 'returns a user\'s serialize_game' do
+        get :show, params: { id: game.id, token: user.token }, format: :json
 
-      expect(response.status).to eq 200
-      expect(JSON.parse(response.body).deep_symbolize_keys[:data][:id]).to eq game.id
-      expect(JSON.parse(response.body).deep_symbolize_keys[:data][:type]).to eq 'game'
+        expect(response.status).to eq 200
+        expect(JSON.parse(response.body).deep_symbolize_keys[:data][:id]).to eq game.id
+        expect(JSON.parse(response.body).deep_symbolize_keys[:data][:type]).to eq 'game'
+      end
+    end
+
+    context 'when the game does not exist' do
+      let(:email) { Faker::Internet.email }
+      let(:password) { 'password' }
+      let(:first_name) { Faker::Name.first_name }
+      let(:last_name) { Faker::Name.last_name }
+      let(:token) { 'token' }
+
+      let!(:user) {
+        User.create(email: email,
+        password: password,
+        firstName: first_name,
+        lastName: last_name,
+        approved: true,
+        token: token)
+      }
+
+      it 'returns a 404' do
+        expect{
+          get :show, params: { id: Faker::Number.number(4), token: user.token }, format: :json
+        }.to raise_exception(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
