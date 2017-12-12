51b7673c6d1679c788d6fac8012cb3ef133259e9
Chad Ellison <chad.ellison0123@gmail.com>
2017-12-11 22:49:25 -0600

    refactor

--- app/models/concerns/ai_logic.rb
+++ app/models/concerns/ai_logic.rb
@@ -22,8 +22,7 @@ module AiLogic
       setup = move_rank.next_positions.where('value < ?', 0).order('value').first
     end
 
-    count = rand(10)
-    setup = move_rank.next_positions.where(value: 0).order('RANDOM()').first if setup.blank? && count < 8
+    setup = move_rank.next_positions.where(value: 0).order('RANDOM()').first if setup.blank? && rand(10) < 8
     setup.move_data(move_rank.position_signature) if setup.present?
   end
 
